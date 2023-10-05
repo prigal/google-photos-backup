5d4dd0afa5ab157492ce84fe16186da44675e2c4
Casper Verswijvelt <casperverswijvelt@gmail.com>
2023-10-06 00:51:54 +0200

    Fix docker cron not using correct env vars

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+printenv | grep -v "no_proxy" >> /etc/environment
+
 if [ -n "$CRON" ]; then
-    echo "$CRON cd /usr/src/app/ && bash job.sh >> /var/log/cron.log 2>&1" > /etc/cron.d/sync_job
+
+    echo "$CRON cd /usr/src/app/ && bash job.sh > /proc/1/fd/1 2>&1" > /etc/cron.d/sync_job
     chmod 0755 /etc/cron.d/sync_job
     crontab /etc/cron.d/sync_job
     echo "Starting Google Photo Sync in cron mode"
-    cron
-    tail -f /var/log/cron.log
+    cron -f
 else
     bash job.sh
 fi
